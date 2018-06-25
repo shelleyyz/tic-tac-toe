6275c4a67a7eaedbb65815fae1b26e800cd45ad9
Shelley Zhang <shelleyyz91@gmail.com>
2018-06-25 20:14:48 +1000

    saving changes

--- js/main.js
+++ js/main.js
@@ -5,15 +5,52 @@
 
 //create function for user clicking a cell
 //create function to check for match & game over
-//
 
 console.log('testing');
 
+let switchPlayer = true;
 
 const board = [0,0,0,0,0,0,0,0,0];
+//Initiate the game
 
-const playerSwitch = true;
 
-const playTurn = function(number) {
-  board[number] = "x"; //number
+
+//Keep track of whose turn it is
+
+//Create players
+
+
+const playerOne = function(index) {
+  board[index] = "X";
 }
+
+const playerTwo = function(index) {
+  board[index] = "O";
+}
+
+const playTurn = function(index) {
+  if (switchPlayer === true) {
+    playerOne(index);
+    switchPlayer = false;
+  } else {
+    playerTwo(index);
+    switchPlayer = true;
+  };
+}
+
+//create function to check whether a player has won
+//create a function for when a cell is clicked
+//create a function for restart the game
+//create a function to load the game up
+
+const checkForMatch = function() {
+
+}
+
+//create function to check whether
+//
+// $(document).ready(function() {
+//
+//
+//
+// }
