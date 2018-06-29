3fbdfabc1d4ddff4052b1b124096d87468cb642e
Shelley Zhang <shelleyyz91@gmail.com>
2018-06-29 11:36:08 +1000

    last commit

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 let nextPlayerTurn = true;
 let turnCounter = 0;
 let boxId = 0;
+let xWin = 0;
+let oWin = 0;
+
 const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
-const renderBoardToScreen = function(boxId) { 
+const renderBoardToScreen = function(boxId) {
   $(`#${boxId}`).html("<p>" + (board[boxId]) + "</p>");
   $(`#${boxId} p`).hide().fadeIn(1000);
+  $('#x').html("X wins: " + xWin);
+  $('#o').html("O wins: " + oWin);
 };
+
 
 const playerOne = function(index) {
   board[index] = "X";
@@ -32,47 +38,87 @@ const playTurn = function(index) {
     turnCounter = turnCounter + 1;
     checkForMatch();
     renderBoardToScreen(index);
-  }
+    }
 };
 
 const checkForMatch = function() {
   if (board[0] === board[1] && board[1] === board[2] && board[0] !== " ") {
     $('.playerWon').html(`${board[0]} has won!`)
+    if ( board[0] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true
   } else if (board[3] === board[4] && board[4] === board[5] && board[3] !== " ") {
     $('.playerWon').html(`${board[3]} has won!`)
+    if ( board[3] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[6] === board[7] && board[7] === board[8] && board[6] !== " ") {
     $('.playerWon').html(`${board[6]} has won!`)
+    if ( board[6] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[0] === board[3] && board[3] === board[6] && board[0] !== " ") {
     $('.playerWon').html(`${board[0]} has won!`)
+    if ( board[0] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[1] === board[4] && board[4] === board[7] && board[1] !== " ") {
     $('.playerWon').html(`${board[1]} has won!`)
+    if ( board[1] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[2] === board[5] && board[5] === board[8] && board[2] !== " ") {
     $('.playerWon').html(`${board[2]} has won!`)
+    if ( board[2] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[0] === board[4] && board[4] === board[8] && board[0] !== " ") {
     $('.playerWon').html(`${board[0]} has won!`)
+    if ( board[0] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
   } else if (board[2] === board[4] && board[4] === board[6] && board[2] !== " ") {
     $('.playerWon').html(`${board[2]} has won!`)
+    if ( board[2] === "X" ) {
+      xWin ++;
+    } else {
+      oWin ++
+    }
     $('.box').off("click");
     $('.popup').show(1000);
     return true;
